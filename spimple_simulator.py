6bf04e5201e555cc4f32be2f0de9b46c4c70e891
jepeng <jepeng2019@qq.com>
2026-02-26 15:15:03 -0500

    add obstacle_pos

--- spimple_simulator.py
+++ spimple_simulator.py
@@ -9,10 +9,11 @@ import random
 robot_name = "Jepeng H1"
 battery = 100.0
 position = 0.0
+obstacle_pos = random.uniform(1.0,20.0)
 is_running = True
 
 print(f"---system start{robot_name}---")
-
+print(f"---obstacle is in {obstacle_pos:.1f}m ---")
 def move_forward(current_pose, speed):
     noise = random.uniform(-0.1, 0.1)
     distance = speed + noise
@@ -25,6 +26,10 @@ while is_running:
         is_running = False
         break
     position = move_forward(position, 1.5)
+    if(position>=obstacle_pos):
+        print("[WARNING] Obstacle! Stop!")
+        is_running = False
+        break
     battery -= 5.0
     print(f"[Log] Pose: {position:.2f} m | Battery: {battery:.1f}% | Status: Running")
     time.sleep(0.5)
